048cf1143bf26e81c839e0e8ad8e6146ac1ceabe
marceloaraujo14 <mbaraujo1406@gmail.com>
2022-04-01 19:50:00 -0300

    Configuração para buildar imagem e rodar a aplicação do container

--- cliente/src/main/java/com/tqibank/cliente/domain/entities/Client.java
+++ cliente/src/main/java/com/tqibank/cliente/domain/entities/Client.java
@@ -1,15 +1,18 @@
 package com.tqibank.cliente.domain.entities;
 
 import lombok.*;
+import org.hibernate.Hibernate;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import java.math.BigDecimal;
+import java.util.Objects;
 
 @Entity
 @Builder
-@EqualsAndHashCode(onlyExplicitlyIncluded = true)
-@Data
+@Getter
+@Setter
+@ToString
 @AllArgsConstructor
 @NoArgsConstructor
 public class Client {
@@ -23,4 +26,17 @@ public class Client {
     private String rg;
     private BigDecimal income;
     private String adress;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Client client = (Client) o;
+        return email != null && Objects.equals(email, client.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
